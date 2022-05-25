5d1367b76bbd3b8b78abe689cf0e5ace3620596c
ridhaaa325 <ridhasamudra@upi.edu>
2022-05-25 16:25:57 +0700

    Co-authored-by: Raihan "VieR" Fitrah <vier15@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,17 +1,49 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gas Lickage Detecio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Test</h1>
-    <p>Awaikawkawok</p>
-    <!-- Ridha tidak baik. Ridha sombong. #PukulRidha -->
+    <section>
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="./Assets/rhodes_island_black.png" alt="" width="36" height="30"
+                        class="d-inline-block align-text-top">
+                    Gas Lickage Detecio
+                </a>
+            </div>
+        </nav>
+    </section>
+
+    <section>
+        <img src="/Assets/amiya.gif" alt="" class="img-fluid mx-auto d-block mt-5">
+        <figcaption class="image-footer text-center">
+            Rhodes Island does not avenge the dead, but it will work let the living live. <br><cite title="Source Title">-Arknights</cite>
+        </figcaption>
+    </section>
+<!-- Ridha tidak baik. Ridha sombong. #PukulRidha -->
+    <section>
+        <div class="mx-auto text-center mb-5">
+            <h3>Detection Status: <span></span></h3>
+            <button type="button" class="btn btn-success">Success</button>
+        </div>
+
+        <div class="text-center">
+            Turn Alarm
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
